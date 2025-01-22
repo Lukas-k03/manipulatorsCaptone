fcad68340042a872773fbed3a72397b5a7583755
YourLocalSock <117708234+YourLocalSock@users.noreply.github.com>
2025-01-22 11:58:30 -0500

    Base Outline for GUI
    Added comments that show the initial plan for the claw's buttons.

--- guiInterface/GUIInterface.py
+++ guiInterface/GUIInterface.py
@@ -1,1 +1,15 @@
+<<<<<<< Updated upstream
 import numpy as np
+=======
+#inport all neccessary packages
+
+
+
+#buttons for base movement (clockwise / counter-clockwise)
+
+
+#buttons for joint movement (up / down)
+
+
+#buttons for claw movement (up / down)
+>>>>>>> Stashed changes
